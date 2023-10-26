0b6cd1efcc189e70b4a3f0a1e788241cf9b203d8
sayanrick <sayanrick100@gmail.com>
2023-10-26 23:58:02 +0530

    Integrating Typescript with Node and Express

---
+++ routes/todos.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+import { Todo } from '../models/todos';
+
+let todos: Todo[] = [];
+const router = Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+
+router.post('/todo', (req, res, next) => {
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+
+    todos.push(newTodo);
+    res.status(201).json({ message: 'Added Todo', todo: newTodo, todos: todos });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: tid, text: req.body.text };
+        res.status(200).json({ message: 'Updated Todo', todo: todos[todoIndex], todos: todos });
+    } else {
+        res.status(404).json({ message: 'Could not find todo for this id.' });
+    }
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const initialLength = todos.length;
+    todos = todos.filter(todoItem => todoItem.id !== tid);
+
+    if (todos.length < initialLength) {
+        res.status(200).json({ message: 'Deleted todo', todos: todos });
+    } else {
+        res.status(404).json({ message: 'Could not find todo for this id.' });
+    }
+});
+
+export default router;
